4a60743ef779fb5be27094e6761353669e66de76
Fahad Qureshi <qureshifahad984@gmail.com>
2026-08-11 22:48:17 +0500

    Add files via upload

---
+++ Jobs_Recommendation_System/utils/resume_parser.py
@@ -0,0 +1,21 @@
+# Importing PdfReader
+
+from pypdf import PdfReader
+
+# Building Resume Extraction Text Data from Resume
+
+def extract_text_from_pdf(uploaded_file):
+
+    reader = PdfReader(uploaded_file)
+
+    text = ""
+
+    for page in reader.pages:
+        page_text = page.extract_text()
+
+        if page_text:
+            text += page_text + "\n"
+
+
+    return text
+
